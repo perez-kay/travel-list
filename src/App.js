d39eeafd606e05a346cd633e709ee6f20bfd24f9
Kayla Perez <perezkay@uw.edu>
2024-06-27 12:07:05 -0700

    created layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import './App.css';
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer>You have X items on your list and you already packed X (x%)</footer>
+  );
 }
 
 export default App;
