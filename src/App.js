84d511b857892bc52227cbec3465d578c34b549e
Kayla Perez <perezkay@uw.edu>
2024-06-27 12:18:47 -0700

    rendering list items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
 function App() {
   return (
     <div className="app">
@@ -22,8 +27,29 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" name="" id="" />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
